d7836d8fc8e27249ade1bafd8bb0748dcfd5aa09
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-10-08 13:33:26 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#381181]

---
+++ chapter_001/src/main/java/Iterator/EvenIt.java
@@ -0,0 +1,38 @@
+package Iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenIt implements Iterator<Integer> {
+    private final int[] numbers;
+    private int point = 0;
+
+    public EvenIt(final int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public boolean hasNext() {
+        int evenIndex = 0;
+        boolean isEven = false;
+        for (int i = point; i < numbers.length; i++) {
+                if(numbers[i] %2 == 0){
+                    isEven = true;
+                    evenIndex = i;
+                }
+                point = evenIndex;
+        }
+        return isEven;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        return numbers[];
+    }
+}
+
+
