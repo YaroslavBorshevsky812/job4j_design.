fc63e6689a5d00b03de439258426aa7ad8313bd0
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-10-12 16:59:19 +0300

    5.2.1. Реализовать SimpleArray<T> [#381192]

---
+++ chapter_001/src/main/java/Generics/AppStore/Base.java
@@ -0,0 +1,14 @@
+package Generics.AppStore;
+
+
+public abstract class Base {
+    private final String id;
+
+    protected Base(final String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ chapter_001/src/main/java/Generics/AppStore/User.java
@@ -0,0 +1,8 @@
+package Generics.AppStore;
+
+public class User extends Base{
+
+    protected User(String id) {
+        super(id);
+    }
+}
