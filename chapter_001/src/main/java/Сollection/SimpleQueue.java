4015bfa6e8208c010e726b02abdde924a40522db
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-10-19 13:45:49 +0300

    5. Очередь на двух стеках [#381173]

--- chapter_001/src/main/java/Сollection/SimpleQueue.java
+++ chapter_001/src/main/java/Сollection/SimpleQueue.java
@@ -1,29 +1,19 @@
 package Сollection;
 
 public class SimpleQueue<T> {
-    private int size = 0;
     private final SimpleStack<T> s1 = new SimpleStack<>();
     private final SimpleStack<T> s2 = new SimpleStack<>();
 
     public T pop() {
-        T buf = null;
-        if(!s2.isEmpty()){
-            buf = s2.pop();
-            size--;
-        } else {
-            if (!s1.isEmpty()) {
-                for (int i = 0; i < size; i++) {
-                    s2.push(s1.pop());
-                }
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
             }
-            buf = s2.pop();
-            size--;
         }
-        return buf;
+        return s2.pop();
     }
     public void push(T value) {
         s1.push(value);
-        size++;
     }
 
     public static void main(String[] args) {
@@ -33,6 +23,5 @@ public class SimpleQueue<T> {
         queue.pop();
         queue.push(3);
         System.out.println(queue.pop());
-        System.out.println(queue.size);
     }
 }
