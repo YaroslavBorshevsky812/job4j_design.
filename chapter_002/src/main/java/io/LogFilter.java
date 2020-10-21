a117aa014f0acc9b180683061e5a4de4e8cb72bd
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-10-21 18:24:00 +0300

    0.4. BufferedOutputStream [#381221]

--- chapter_002/src/main/java/io/LogFilter.java
+++ chapter_002/src/main/java/io/LogFilter.java
@@ -14,8 +14,8 @@ public class LogFilter {
             for (String line : lines) {
                 String[] split = line.split(" ");
                 if ("404".equals(split[split.length - 2])) {
-                    System.out.println(line);
-                    line1.add(line);
+
+                    line1.add(line + "\n");
                 }
             }
         } catch (Exception e) {
@@ -23,9 +23,21 @@ public class LogFilter {
         }
         return line1;
     }
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("logResult.txt")
+                ))) {
+            out.write(filter("log.txt" + "\n").toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
+        save(log, "logResult.txt");
+
     }
 }
