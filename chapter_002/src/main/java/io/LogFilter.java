233518f062b871778989f85871bdf6ce9afd15fe
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-10-22 15:51:00 +0300

    0.3. BufferedReader. [#381220]

--- chapter_002/src/main/java/io/LogFilter.java
+++ chapter_002/src/main/java/io/LogFilter.java
@@ -1,43 +1,40 @@
 package io;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogFilter {
     public static List<String> filter(String file) {
         List<String> line1 = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             List<String> lines = new ArrayList<String>();
-            in.lines().forEach(lines::add);
-            for (String line : lines) {
-                String[] split = line.split(" ");
-                if ("404".equals(split[split.length - 2])) {
-
-                    line1.add(line + "\n");
-                }
-            }
+           line1 = in.lines()
+                    .filter(s -> s.contains("404")).collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return line1;
     }
-    public static void save(List<String> log, String file) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream("logResult.txt")
-                ))) {
-            out.write(filter("log.txt" + "\n").toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void save(List<String> log, String file) {
+//        try (PrintWriter out = new PrintWriter(
+//                new BufferedOutputStream(
+//                        new FileOutputStream("logResult.txt")
+//                ))) {
+//            out.write(filter("log.txt" + "\n").toString());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
-        save(log, "logResult.txt");
+//        save(log, "logResult.txt");
 
     }
 }
