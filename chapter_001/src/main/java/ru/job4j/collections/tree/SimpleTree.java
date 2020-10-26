07c971c9d5fde77f9c2193389b9dbe3562c34787
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-10-26 16:50:50 +0300

    1. Создать элементарную структуру дерева [#381188]

---
+++ chapter_001/src/main/java/ru/job4j/collections/tree/SimpleTree.java
@@ -0,0 +1,28 @@
+package ru.job4j.collections.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface SimpleTree<E> {
+
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+        public void add(Node<E> child) {
+            this.children.add(child);
+        }
+
+        public List<Node<E>> getChildren() {
+            return children;
+        }
+    }
+}
