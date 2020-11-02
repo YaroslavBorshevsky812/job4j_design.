e077bccb5147908ba28e845c8bdd0d6af0db225c
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-11-02 11:38:18 +0300

    4.1. Сканирование файловой системы. [#381216]

---
+++ chapter_002/src/main/java/io/SearchFiles.java
@@ -0,0 +1,43 @@
+package io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+    private Predicate<Path> pathPredicate;
+    private List<Path> listOfPath = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> predicate) {
+        this.pathPredicate = pathPredicate;
+    }
+
+    public List<Path> getPaths() {
+        return this.listOfPath;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return null;
+    }
+}
