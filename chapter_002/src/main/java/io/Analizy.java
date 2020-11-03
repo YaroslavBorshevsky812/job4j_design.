8c1d40019e89cfdf776642cce17420deb2878788
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-11-03 17:08:21 +0300

    2. Анализ доступности сервера. [#381226]

--- chapter_002/src/main/java/io/Analizy.java
+++ chapter_002/src/main/java/io/Analizy.java
@@ -9,29 +9,28 @@ import java.util.stream.Stream;
 public class Analizy {
 
     public static void unavailable(String source, String target) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(source));
-            PrintWriter writer = new PrintWriter(
-                    new BufferedOutputStream(new FileOutputStream(target)));
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(source));
+            PrintWriter writer = new PrintWriter(new BufferedOutputStream(
+                    new FileOutputStream(target)))) {
             String serverDown = null;
             while (reader.ready()) {
-                String s = reader.readLine();
-                if (s.startsWith("400") || s.startsWith("500")) {
-                    String[]s1 = s.split(" ");
-                    writer.println(s1[1]);
-                } else {
-                    if (!(s.startsWith("500")) || !s.startsWith("400")) {
-                        String[]s1 = s.split(" ");
-                        writer.println(s1[1]);
-                    }
+                String status = reader.readLine();
+                if (serverDown == null && (status.startsWith("400") || status.startsWith("500"))) {
+                    writer.write(status.split(" ")[1] + ";");
+                    serverDown = status;
+                } else if (serverDown != null && (!status.isEmpty()
+                        && !status.startsWith("400") && !status.startsWith("500"))) {
+                    writer.write(status.split(" ")[1]);
+                    serverDown = null;
+                    writer.println();
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
     public static void main(String[] args) {
             unavailable("server.log", "resultServer.csv");
         }
