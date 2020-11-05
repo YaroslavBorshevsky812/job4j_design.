e4195ae0c2805c05e85ee973991281f343bd918e
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-11-05 18:58:49 +0300

    1. Бот [#381242]

--- chapter_002/src/main/java/io/EchoServer.java
+++ chapter_002/src/main/java/io/EchoServer.java
@@ -3,6 +3,7 @@ package io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.Stream;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -12,14 +13,22 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("?msg=Bye ")) {
-                            server.close();
+                    String str = in.readLine();
+                    if (!str.isEmpty()) {
+                        str = Stream.of(str.split(" "))
+                                .filter(s -> s.contains("msg"))
+                                .findFirst()
+                                .orElse("");
+                    }
+                    System.out.println("msg: " + str);
+                    if (!str.isEmpty()) {
+                        String[] arrStr = str.split("=");
+                        str = arrStr[1];
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".concat(str).getBytes());
+                        if (str.equals("Exit")) {
+                            break;
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
                 }
             }
         }
